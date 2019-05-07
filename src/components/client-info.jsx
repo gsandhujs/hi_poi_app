9716d9c0aa97531dc8186ac44bf89d091cd55cbe
gursherb <gsandhu.js@gmail.com>
2019-05-06 22:22:52 -0400

    more ui enhancement for displaying checkpoints

--- src/components/client-info.jsx
+++ src/components/client-info.jsx
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 const ClientInfo = (props) =>{
     return(
-        <div>
+        <div className="flex">
             {
                 ( props.current_client_info.pointsOfInterest || [] ).map(poi=>{
                     return (
-                    <div key={Math.random()}>
+                    <div key={Math.random()} className="client-info">
                         <label>Floor: </label><span>{poi.floor}</span><br/>
                         <label>Latitude: </label><span>{poi.latitude}</span><br/>
                         <label>Longitude: </label><span>{poi.longitude}</span>
